6352a6aafacd1c1f485d389c4ac88eb2b95ee2dd
Raúl Juárez <raul.juarez.7e8@itb.cat>
2024-09-26 16:01:46 +0200

    update1

--- comprovarEdat.py
+++ comprovarEdat.py
@@ -1,13 +1,13 @@
 
-
 name = str(input("Hola, quin es el teu nom? "))
 edat = int(input("Quina edat tens? "))
+
+# Preguntamos la edad e indicamos si es mayor o menor de edad.
 
 if edat >= 18:
     print('Hola ' + name + ', ets major de edat')
 else:
     print('Hola ' + name + ', ets menor de edat')
 
-# Finalizamos programa
 print("Programa finalitzat")
 
