387dba821f848afdbf586c337fcd71b111ed2e02
RaulJuarez-ITB2425 <raul.juarez.7e8@itb.cat>
2024-10-03 10:46:00 +0200

    update1

--- Ejercicios extra/ej5.py
+++ Ejercicios extra/ej5.py
@@ -6,4 +6,6 @@ print(str(juan)+", "+str(maria)+", "+str(adan))
 total_pomes = int(juan+maria+adan)
 print(total_pomes)
 
-print($"Nombre total de pomes: ")
+print("Nombre total de pomes:",str(juan), "+",str(maria),"+",str(adan),"=",str(total_pomes))
+
+print(f"Nombre total de pomes: {juan} + {maria} + {adan} = {total_pomes}")
