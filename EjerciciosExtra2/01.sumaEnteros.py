418b196a8486bd1da733154fa2a0fa1c0706ed2c
Raúl Juárez <raul.juarez.7e8@itb.cat>
2024-10-11 08:40:49 +0200

    commit1

--- EjerciciosExtra2/01.sumaEnteros.py
+++ EjerciciosExtra2/01.sumaEnteros.py
@@ -2,8 +2,9 @@ while True:
     try:
         n1 = int(input("Escribe un entero: "))
         n2 = int(input("Escribe otro entero: "))
+        break
     except ValueError:
-        print("Tienes que escribir un número entero")
+        print("Tienes que escribir números enteros")
 
 n3 = n1+n2
 
