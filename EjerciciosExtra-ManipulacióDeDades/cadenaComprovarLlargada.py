66812df4d2f81fb6ddc5965f533b642ed1d24fc1
Raúl Juárez <raul.juarez.7e8@itb.cat>
2024-10-17 21:33:28 +0200

    update1

---
+++ EjerciciosExtra-ManipulacióDeDades/cadenaComprovarLlargada.py
@@ -0,0 +1,3 @@
+texto1 = input(str("Escribe un texto cualquiera para comprobar su número de caracteres: "))
+long_texto1 = len(texto1)
+print(f"La longitud de tu texto es: {long_texto1}")
---
+++ EjerciciosExtra-ManipulacióDeDades/cadenaConcatenar.py
@@ -0,0 +1,4 @@
+texto1 = input(str("Escribe un texto cualquiera para concatenarlo a otro: "))
+texto2 = input(str("Escribe el texto al que lo quieres concatenar: "))
+con_texto12 = texto1 + " " +texto2
+print(f"El texto concatenado es: {con_texto12}")
